050698dbca88a4722e7579df52a18dc73e5eeeeb
Haiku-na <appoline.vautrin@gmail.com>
2025-10-25 15:15:41 +0200

    update formation / exp pro

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
               </span>
             </a>
 
-            <button type="button" onClick={toggleMenu} className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded={isMenuOpen} >
+            <button type="button" onClick={toggleMenu} className="cursor-pointer inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600 transition" aria-controls="navbar-sticky" aria-expanded={isMenuOpen} >
               <span className="sr-only">Ouvrir le menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14" >
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -41,22 +41,22 @@ export default function Header() {
           <div className={`${isMenuOpen ? "" : "hidden"} items-center justify-between w-full md:flex md:w-auto md:order-1`} id="navbar-sticky" >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-gray-900 md:bg-gray-900 border-gray-700">
               <li>
-                <Link to="/" className="block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white" onClick={() => setIsMenuOpen(false)} >
+                <Link to="/" className="block py-2 px-3 rounded-sm hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white transition" onClick={() => setIsMenuOpen(false)} >
                   Accueil
                 </Link>
               </li>
               <li>
-                <Link to="/portfolio/qui_je_suis" className="block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white" onClick={() => setIsMenuOpen(false)}>
+                <Link to="/portfolio/qui_je_suis" className="block py-2 px-3 rounded-sm hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white transition" onClick={() => setIsMenuOpen(false)}>
                   Portfolio
                 </Link>
               </li>
               <li>
-                <a href="https://cv-numerique-wheat.vercel.app/" target="_blank" rel="noopener noreferrer" className="block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white" onClick={() => setIsMenuOpen(false)}>
+                <a href="https://cv-numerique-wheat.vercel.app/" target="_blank" rel="noopener noreferrer" className="block py-2 px-3 rounded-sm hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white transition" onClick={() => setIsMenuOpen(false)}>
                   CV Numérique
                 </a>
               </li>
               <li>
-                <Link to="/contact" className="block py-2 px-3 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white" onClick={() => setIsMenuOpen(false)}>
+                <Link to="/contact" className="block py-2 px-3 rounded-sm hover:bg-gray-700 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-white" onClick={() => setIsMenuOpen(false)}>
                   Contact
                 </Link>
               </li>
